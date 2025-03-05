c3e1c998a6e894d9c954696933b4606500f7a887
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-03-05 23:55:27 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,1 @@
-"use client";
-import { useState } from "react";
 
-export default function Home() {
-  const [address1, setAddress1] = useState("");
-  const [address2, setAddress2] = useState("");
-  const [result, setResult] = useState(null);
-
-  
